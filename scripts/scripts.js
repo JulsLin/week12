a317852e58407bca3082b223409d47dfa7efb383
Juls Lin <julianna.lin4@gmail.com>
2023-03-27 15:47:44 -0700

    change bg to pink and text content everytime it is clicked

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -6,3 +6,22 @@ function alertUser() {
 }
 
 buttonRef.addEventListener("click", alertUser, {once:true});
+
+function changeBGPink() {
+    document.body.style.backgroundColor = 'pink';
+    //document.body.classList.add("pinkBG");
+}
+buttonRef.addEventListener("click", changeBGPink);
+
+
+
+function changeText () {
+    // if the button says click me change the textcontent to clicked!
+    //else if it says "Clicked!" update it to "Click Me!"
+    if (buttonRef.textContent === "Click Me!"){
+    buttonRef.textContent = "Clicked!";
+    } else {
+        buttonRef.textContent = "Click Me!"
+    }
+}
+buttonRef.addEventListener("click", changeText);
